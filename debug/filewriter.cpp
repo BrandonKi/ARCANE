27a5412c8109c315572d46fb8ff35b0401fdca04
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-04-28 12:52:07 -0400

    Update filewriter.cpp

--- debug/filewriter.cpp
+++ debug/filewriter.cpp
@@ -14,17 +14,31 @@ int main(){
     // };
      unsigned char data[] = 
     {
+        0x42, // start file signature
+        0x72,
+        0x61,
+        0x6e,
+        0x64,
+        0x6f,
+        0x6e,
+        0x4b,
+        0x69,
+        0x72,
+        0x69,
+        0x6e,
+        0x63,
+        0x69,
+        0x63,
+        0x68, // end file signature
+
         0x11,
-        0xff,
-        0xfc,
-        0xff,
-        0xff,
+        0x02,
         0x11,
-        0xff,
-        0xff,
-        0xff,
-        0xff,
-        0xa2
+        0x01,
+        0xa1,
+
+        0x01, // exit
+        0x00  // with code 0
     };
 
     std::ofstream file("C:\\Users\\Kirin\\Desktop\\VM\\src\\example.dat", std::ios::out|std::ios::binary|std::ios::trunc);
