0bfc6cfca3750db5212e76b617e968de03ad3758
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-05-02 16:02:08 -0400

    CLI

---
+++ bin/run.cmd
@@ -0,0 +1,2 @@
+@echo off
+%~dp0..\src\Main.exe %1
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,36 +1,47 @@
 #include <fstream>
-
+#include <chrono> 
 #include "VM.h"
 
-bool checkFileType(char* data);
+clock_t start, end; 
+#define TIMER_START start = clock()
+#define TIMER_STOP std::cout << "Time taken by program is : " << (double(clock() - start) / double(CLOCKS_PER_SEC)) * 1000 << " milliseconds " << std::endl; 
 
-int main () {
-  std::streampos size;
-  std::ifstream file ("example.dat", std::ios::in|std::ios::binary|std::ios::ate);
-  char* filedata;
-  if (file.is_open()){
-    size = file.tellg();
-    filedata = new char [size];
-    file.seekg (0, std::ios::beg);
-    file.read (filedata, size);
-    file.close();
-    if(checkFileType(filedata)){
-      std::cout << "FILE VERIFIED\n";
-      std::cout << filedata << "\n";
-      filedata = filedata+(16 * sizeof(char));
-      std::cout << filedata <<  " " << (uint)size-16 << "\n";
-      VM vm(filedata, (uint)size-16);
-      vm.run();
-    }
-  }else{
-    std::cout << "Unable to open file";
-    return 0;
-  }
-  std::cin.get();
-  return 0;
+inline bool checkFileType(char* data);
+inline char* appendExtension(const char* filename);
+
+int main (int argc, const char* argv[]) {
+  if(argc > 1){
+    TIMER_START;
+    std::streampos size;
+    std::ifstream file (appendExtension(argv[1]), std::ios::in|std::ios::binary|std::ios::ate);
+    char* filedata;
+    if (file.is_open()){
+      size = file.tellg();
+      filedata = new char [size];
+      file.seekg (0, std::ios::beg);
+      file.read (filedata, size);
+      file.close();
+      if(checkFileType(filedata)){
+        filedata = filedata + (16 * sizeof(char));
+        VM vm(filedata, (uint)size-16);
+        vm.run();
+        TIMER_STOP;
+        return 0;
+      }else
+        ERR("FILE_FORMAT_NOT_RECOGNIZED");
+    }else
+      ERR("UNABLE_TO_OPEN_FILE");
+  }else
+    ERR("NO_FILE_PROVIDED");
 }
 
-bool checkFileType(char* data){
-  const char* sig = "BrandonKirincich";
-  return memcmp(sig, data, 16) == 0;
+inline bool checkFileType(char* data){
+  return memcmp("BrandonKirincich", data, 16) == 0;
+}
+
+inline char* appendExtension(const char* filename){
+  char* result = new char[sizeof(filename)+3];
+  memcpy(result, filename, sizeof(filename));
+  memcpy(&result[sizeof(filename)-1], ".dat", 4);
+  return result;
 }
