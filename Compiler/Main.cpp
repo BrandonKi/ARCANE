7956b51135c74a141aa5dc05a3c043b2440b2c2f
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-05-06 20:57:17 -0400

    Initial commit

---
+++ Compiler/Main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+#define ERR(x) std::cout << x << "\n"; return -1
+
+inline bool checkFileType(const char* temp);
+
+int main(int argc, const char* argv[]){
+    for(int i = 0; i < argc; i++)
+        std::cout << argv[i] << "\n";
+    std::cout << sizeof(argv[1]) << "\n";
+    std::cout << strlen(argv[1]) << "\n";
+    if(argc > 1){
+        std::streampos size;
+        std::ifstream file (argv[1], std::ios::in|std::ios::ate);
+        char* filedata;
+        if (file.is_open()){
+            size = file.tellg();
+            filedata = new char [size];
+            file.seekg (0, std::ios::beg);
+            file.read (filedata, size);
+            file.close();
+            if(checkFileType(argv[1])){
+                std::cin.get();
+                filedata = filedata + (16 * sizeof(char));
+                return 0;
+            }else
+                ERR("FILE_FORMAT_NOT_RECOGNIZED");
+        }else
+            ERR("UNABLE_TO_OPEN_FILE");
+    }else
+        ERR("NO_FILE_PROVIDED");
+}
+
+inline bool checkFileType(const char* data){
+    int len = strlen(data) - 4;
+    const char* temp = &data[len];
+    return memcmp("arcs", temp, 4) == 0;
+}
