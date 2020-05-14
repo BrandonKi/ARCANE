d063b25738dc11d1b626b8cb220283d4981b08ea
Brandon Kirincich <44515121+BrandonKi@users.noreply.github.com>
2020-05-13 23:42:27 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-// #include <sys/mman.h>
-#include <memoryapi.h>
+#include <cstring>
+#include <sys/mman.h>
+//#include <memoryapi.h>
 
-// Allocates RWX memory of given size and returns a pointer to it. On failure,
+// Allocates RW memory of given size and returns a pointer to it. On failure,
 // prints out the error and returns NULL.
 void* alloc_memory(size_t size) {
-  // void* ptr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);     // linux
-  void* ptr = VirtualAlloc(NULL, size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);               // windows
+  void* ptr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);     // linux
+  //void* ptr = VirtualAlloc(NULL, size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);               // windows
   if (ptr == (void*)-1) {
     std::cerr << "ALLOC_ERROR";
     return NULL;
@@ -15,29 +16,29 @@ void* alloc_memory(size_t size) {
 }
 
 void emit_code_into_memory(void* m) {
-  // unsigned char code[] = {
-  //   0x48, 0x89, 0xf8,                   // mov rax, rdi
-  //   0x48, 0x83, 0xc0, 0x04,             // add rax, 4          // linux
-  //   0xc3                                // ret
-  // };
-  unsigned char code[] = {
-    0x48, 0x89, 0xc8,                   // mov rax, rcx
-    0x48, 0x83, 0xc0, 0x08,             // add rax, 4            // windows
-    0xc3                                // ret
-  };
+   unsigned char code[] = {
+     0x48, 0x89, 0xf8,                   // mov rax, rdi
+     0x48, 0x83, 0xc0, 0x08,             // add rax, 4          // linux
+     0xc3                                // ret
+   };
+  //unsigned char code[] = {
+  // 0x48, 0x89, 0xc8,                   // mov rax, rcx
+  // 0x48, 0x83, 0xc0, 0x08,             // add rax, 4            // windows
+  // 0xc3                                // ret
+  //};
   memcpy(m, code, sizeof(code));
 }
 
 void make_code_executable(void* buf){
 
-    // mprotect(buf, sizeof(*buf), PROT_READ | PROT_EXEC);            // linux
+    mprotect(buf, sizeof(*(char*)buf), PROT_READ | PROT_EXEC);            // linux
     
-    DWORD old;
-    VirtualProtect(buf, sizeof(*(char*)buf), PAGE_EXECUTE_READ, &old);       // windows
+    //DWORD old;
+    //VirtualProtect(buf, sizeof(*(char*)buf), PAGE_EXECUTE_READ, &old);       // windows
 }
 
 const size_t SIZE = 1024;
-typedef long (*JittedFunc)(long);
+typedef long (*exe)(long);
 
 // Allocates RWX memory directly.
 int main() {
@@ -45,7 +46,7 @@ int main() {
   emit_code_into_memory(m);
   make_code_executable(m);
 
-  JittedFunc func = (JittedFunc)m;
+  exe func = (exe)m;
   int result = func(2);
   printf("result = %d\n", result);
   std::cin.get();
