bf971a17df0631e6f0154e78aa1704ea28475063
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-05-17 20:52:28 -0400

    64 bit test

---
+++ ARCVM/src/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+
+int main(){
+    signed char temp[] = {'B', 'R', 'A', 0x00};
+    uint64_t temp1 = (uint64_t)temp;
+    std::cout << std::hex << temp1 << std::dec << "\n";
+    uint32_t temp2 = (temp1 & 0x00000000ffffffff);
+    uint32_t temp3 = (temp1 & 0xffffffff00000000) >> 32;
+    uint64_t result = ((uint64_t)temp3 << 32) | temp2;
+    std::cout << std::hex << result << std::dec << "\n";
+    std::cout << ((signed char*)(result)) << "\n";
+    std::cin.get();
+}
