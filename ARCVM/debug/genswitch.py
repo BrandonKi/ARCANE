7e8a0507a157907ab4b801772dfe6308e267298b
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-05-23 20:56:22 -0400

    started lexer

---
+++ ARCVM/debug/genswitch.py
@@ -0,0 +1,2 @@
+for i in range(33, 127):
+    print("case \'" + chr(i) + "\':\n    break;")
--- Compiler/Main.cpp
+++ Compiler/Main.cpp
@@ -21,7 +21,8 @@ int main(int argc, const char* argv[]){
             file.seekg (0, std::ios::beg);
             file.read (filedata, size);
             file.close();
-            if(checkFileType(argv[1])){
+            if(checkFileType(argv[1], size)){
+                lex(filedata, size);
                 std::cin.get();
                 return 0;
             }else
@@ -32,6 +33,202 @@ int main(int argc, const char* argv[]){
         ERR("NO_FILE_PROVIDED");
 }
 
-inline bool checkFileType(const char* data){   // Makes sure the extension is correct. There is no file signature because it is stored as plain text.
-    return memcmp("arcs", &data[strlen(data) - 4], 4) == 0;
+inline bool checkFileType(const char* data, unsigned int length){   // Makes sure the extension is correct. There is no file signature because it is stored as plain text.
+    return memcmp("arcs", &data[length - 4], 4) == 0;
+}
+
+
+void lex(const char* buf, unsigned int length){
+    for(unsigned int i = 0; i < length; i++){
+        switch(buf[0]){
+            case '!': 
+                break;
+            case '\"': 
+                break;
+            case '#': 
+                break;
+            case '$': 
+                break;
+            case '%': 
+                break;
+            case '&': 
+                break;
+            case '\'': 
+                break;
+            case '(': 
+                break;
+            case ')':
+                break;
+            case '*':
+                break;
+            case '+':
+                break;
+            case ',':
+                break;
+            case '-':
+                break;
+            case '.':
+                break;
+            case '/':
+                break;
+            case '0':
+                break;
+            case '1':
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            case '4':
+                break;
+            case '5':
+                break;
+            case '6':
+                break;
+            case '7':
+                break;
+            case '8':
+                break;
+            case '9':
+                break;
+            case ':':
+                break;
+            case ';':
+                break;
+            case '<':
+                break;
+            case '=':
+                break;
+            case '>':
+                break;
+            case '?':
+                break;
+            case '@':
+                break;
+            case 'A':
+                break;
+            case 'B':
+                break;
+            case 'C':
+                break;
+            case 'D':
+                break;
+            case 'E':
+                break;
+            case 'F':
+                break;
+            case 'G':
+                break;
+            case 'H':
+                break;
+            case 'I':
+                break;
+            case 'J':
+                break;
+            case 'K':
+                break;
+            case 'L':
+                break;
+            case 'M':
+                break;
+            case 'N':
+                break;
+            case 'O':
+                break;
+            case 'P':
+                break;
+            case 'Q':
+                break;
+            case 'R':
+                break;
+            case 'S':
+                break;
+            case 'T':
+                break;
+            case 'U':
+                break;
+            case 'V':
+                break;
+            case 'W':
+                break;
+            case 'X':
+                break;
+            case 'Y':
+                break;
+            case 'Z':
+                break;
+            case '[':
+                break;
+            case '\\':
+                break;
+            case ']':
+                break;
+            case '^':
+                break;
+            case '_':
+                break;
+            case '`':
+                break;
+            case 'a':
+                break;
+            case 'b':
+                break;
+            case 'c':
+                break;
+            case 'd':
+                break;
+            case 'e':
+                break;
+            case 'f':
+                break;
+            case 'g':
+                break;
+            case 'h':
+                break;
+            case 'i':
+                break;
+            case 'j':
+                break;
+            case 'k':
+                break;
+            case 'l':
+                break;
+            case 'm':
+                break;
+            case 'n':
+                break;
+            case 'o':
+                break;
+            case 'p':
+                break;
+            case 'q':
+                break;
+            case 'r':
+                break;
+            case 's':
+                break;
+            case 't':
+                break;
+            case 'u':
+                break;
+            case 'v':
+                break;
+            case 'w':
+                break;
+            case 'x':
+                break;
+            case 'y':
+                break;
+            case 'z':
+                break;
+            case '{':
+                break;
+            case '|':
+                break;
+            case '}':
+                break;
+            case '~':
+                break; 
+        }
+    }
 }
