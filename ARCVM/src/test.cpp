5fe7358eb7fa80f5e61dc04c061deccbf7e787b0
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-05-24 20:36:20 -0400

    update

--- ARCVM/src/test.cpp
+++ ARCVM/src/test.cpp
@@ -3,18 +3,15 @@
 
 
 int main(){
-    // signed char temp[] = {'B', 'R', 'A', 0x00};
-    // uint64_t temp1 = (uint64_t)temp;
-    // std::cout << std::hex << temp1 << std::dec << "\n";
-    // uint32_t temp2 = (temp1 & 0x00000000ffffffff);
-    // uint32_t temp3 = (temp1 & 0xffffffff00000000) >> 32;
-    // uint64_t result = ((uint64_t)temp3 << 32) | temp2;
-    // std::cout << std::hex << result << std::dec << "\n";
-    // std::cout << ((signed char*)(result)) << "\n";
-    // signed char temp = 0xff;
-    // std::cout << (int)temp << "\n";
-    for(int i = 33; i < 127; i++){
-        std::cout << (char)i << "\n";
-    }
+    signed char temp[] = {'B', 'R', 'A', 0x00};
+    uint64_t temp1 = (uint64_t)temp;
+    std::cout << std::hex << temp1 << std::dec << "\n";
+    uint32_t temp2 = (temp1 & 0x00000000ffffffff);
+    uint32_t temp3 = (temp1 & 0xffffffff00000000) >> 32;
+    uint64_t result = ((uint64_t)temp3 << 32) | temp2;
+    std::cout << std::hex << result << std::dec << "\n";
+    std::cout << ((signed char*)(result)) << "\n";
+    signed char temp = 0xff;
+    std::cout << (int)temp << "\n";
     std::cin.get();
 }
