4a52831761a6a530382f4cf54b1e6c7b1289141f
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-06-02 00:20:24 -0400

    moved all common types and includes into one file

--- Compiler/AST.h
+++ Compiler/AST.h
@@ -1,45 +1,4 @@
-#include "Lexer.h"
-
-enum DeclType{D_SBYTE, D_UBYTE, D_SINT, D_UINT, D_FLOAT, D_DOUBLE, D_STRING, D_ARRAY};
-enum ExprType{E_UNARY, E_BINARY, E_TERNARY, E_INTEGER, E_FLOAT, E_STRING, E_INDEX, E_FIELD};
-enum Type{T_INT, T_FLOAT, T_STR, T_ARR};
-
-struct Decl{
-    DeclType type;
-    std::string name;
-    
-    
-};
-
-struct Expr {
-    ExprType type;
-    union{
-        struct {
-            unsigned long long int val;
-        } e_integer;
-        struct {
-            double val;
-        } e_float;
-        struct {
-            std::string val;
-        } e_string;
-        struct {
-            Type type;
-            Expr* index;
-        } e_index;
-        struct {
-            Expr* left;
-            Expr* right;
-            char op;
-        } e_binary;
-        struct {
-            Expr* expr;
-            char op;
-        } e_unary;
-    };
-};
-
-typedef uint_fast64_t uint;
+#include "Parser.h"
 
 class AST{
 
@@ -50,14 +9,16 @@ class AST{
 
     public:
         AST();
-        void start();
+        void init();
+        void runTests();
 
     private:
-        void init();
+        
         void print(Expr*);
         Expr* alloc_expr();
         Expr* newIntegerExpr(uint);
         Expr* newStringExpr(std::string);
         Expr* newUnaryExpr(Expr*, char);
         Expr* newBinaryExpr(Expr*, Expr*, char);
+        Expr* newTernaryExpr(Expr*, Expr*, Expr*);
 };
