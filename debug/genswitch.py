12eb8544b28c0e576fdeec337c38d002b27afd10
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-06-20 17:20:59 -0400

    generate more switch statements

--- debug/genswitch.py
+++ debug/genswitch.py
@@ -1,2 +1,18 @@
+# Generates switch statments so I don't need to write them manually
+
 for i in range(33, 127):
     print("case \'" + chr(i) + "\':\n    break;")
+
+TList = ['T_KEYWORD', 'T_CHAR', 'T_NUMBER', 'T_FLOAT', 'T_STR', 'T_ID', 'T_ASSIGN', 
+            'T_OPERATOR', 'T_COMMA', 'T_DOT', 'T_LPAREN', 'T_RPAREN', 'T_NOT', 
+            'T_LBRACKET', 'T_RBRACKET', 'T_TERNARY', 'T_COLON']
+        
+for i in TList:
+    print("case " + i + ":\n    break;")
+
+TList = [ 'K_AUTO', 'K_CHAR', 'K_UCHAR', 'K_BYTE,' 'K_UBYTE', 'K_INT', 'K_UINT', 
+                    'K_FLOAT', 'K_DOUBLE', 'K_LONG', 'K_STRING', 'K_FOR', 'K_WHILE', 'K_RETURN', 
+                    'K_IF', 'K_ELIF', 'K_ELSE', 'K_SWITCH', 'K_CASE', 'K_BREAK']
+        
+for i in TList:
+    print("case " + i + ":\n    break;")
