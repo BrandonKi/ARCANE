3ad5b12d8445a18e0460651b588c9ff4c2b3d466
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-06-21 19:07:15 -0400

    operator descriptors for tokens

--- Compiler/Token.h
+++ Compiler/Token.h
@@ -46,7 +46,7 @@
 
 #define log(x) std::cout << x
 #define logn(x) std::cout << x << "\n"
-#define ERR(x) std::cout << x << "\n"; std::cin.get(); return -1
+#define ERR(x) std::cout << x << "\n"; std::cin.get(); std::exit(-1)
 
 enum TokenType{ T_KEYWORD, T_CHAR, T_NUMBER, T_FLOAT, T_STR, T_ID, T_ASSIGN, 
                 T_OPERATOR, T_COMMA, T_DOT, T_LPAREN, T_RPAREN, T_NOT, 
@@ -59,14 +59,13 @@ enum KeywordList{   K_AUTO, K_CHAR, K_UCHAR, K_BYTE, K_UBYTE, K_INT, K_UINT,
                     K_IF, K_ELIF, K_ELSE, K_SWITCH, K_CASE, K_BREAK
                 };
 
-enum OperatorDescriptor{    OP_UNARY_NOT, OP_UNARY_INC, OP_UNARY_DEC, OP_LSHIFT, 
-                            OP_RSHIFT, OP_BIN_AND, OP_BIN_OR, OP_BIN_XOR, OP_MUL, 
-                            OP_DIV, OP_MOD, OP_ADD, OP_SUB, OP_EQUAL, OP_NOT_EQUAL, 
-                            OP_LESS_EQUAL, OP_GREAT_EQUAL, OP_GREAT, OP_LESS, 
-                            OP_LOG_AND, OP_LOG_OR, OP_ADD_EQUAL, OP_SUB_EQUAL, 
-                            OP_MUL_EQUAL, OP_DIV_EQUAL, OP_RSHIFT_EQUAL, 
-                            OP_LSHIFT_EQUAL, OP_BIN_AND_EQUAL, OP_BIN_OR_EQUAL,
-                            OP_BIN_XOR_EQUAL,
+enum OperatorDescriptor{    OP_LOG_NOT, OP_UNARY_POST_INC, OP_UNARY_PRE_INC, OP_UNARY_POST_DEC, 
+                            OP_UNARY_PRE_DEC, OP_UNARY_PLUS, OP_UNARY_SUB, OP_LSHIFT, OP_RSHIFT, 
+                            OP_BIN_AND, OP_BIN_OR, OP_BIN_XOR, OP_MUL, OP_DIV, OP_MOD, OP_ADD, 
+                            OP_SUB, OP_EQUAL, OP_NOT_EQUAL, OP_LESS_EQUAL, OP_GREAT_EQUAL, 
+                            OP_GREAT, OP_LESS, OP_LOG_AND, OP_LOG_OR, OP_ADD_EQUAL, OP_SUB_EQUAL, 
+                            OP_MUL_EQUAL, OP_DIV_EQUAL, OP_MOD_EQUAL, OP_RSHIFT_EQUAL, OP_LSHIFT_EQUAL, 
+                            OP_AND_EQUAL, OP_OR_EQUAL, OP_XOR_EQUAL
                         };
 
 struct Token{
