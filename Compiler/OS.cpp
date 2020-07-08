33ac4019116fb30e3d9139638e8a562edd0487d0
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-07-07 20:37:29 -0400

    fix

--- Compiler/OS.cpp
+++ Compiler/OS.cpp
@@ -6,25 +6,29 @@ void OS::init(){
     #endif
 }
 
+
 bool OS::EnableVTMode()
 {
-    // Set output mode to handle virtual terminal sequences
-    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (hOut == INVALID_HANDLE_VALUE)
-    {
-        return false;
-    }
+    #ifdef _WIN32
+        // Set output mode to handle virtual terminal sequences
+        HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+        if (hOut == INVALID_HANDLE_VALUE)
+        {
+            return false;
+        }
 
-    DWORD dwMode = 0;
-    if (!GetConsoleMode(hOut, &dwMode))
-    {
-        return false;
-    }
+        DWORD dwMode = 0;
+        if (!GetConsoleMode(hOut, &dwMode))
+        {
+            return false;
+        }
 
-    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-    if (!SetConsoleMode(hOut, dwMode))
-    {
-        return false;
-    }
-    return true;
+        dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+        if (!SetConsoleMode(hOut, dwMode))
+        {
+            return false;
+        }
+        return true;
+    #endif
+    return false;
 }
