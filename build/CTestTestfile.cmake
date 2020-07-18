ea0db8315799712a345dd4452a6323279ba21418
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-07-18 13:22:50 -0400

    ignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Kirin/Desktop/ARCANE
-# Build directory: C:/Users/Kirin/Desktop/ARCANE/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
