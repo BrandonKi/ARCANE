37f637a6067ab647f990629a8b815ce7ae3cdae8
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-08-27 11:50:29 -0400

    small timer fix again

--- ARCVM/src/VMDriver.cpp
+++ ARCVM/src/VMDriver.cpp
@@ -24,8 +24,8 @@
   #define TIMER_STOP clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end_time);
 
   #define TIMER_PRINT std::cout << "Time taken: " << \
-                      (end_time.tv_sec - start_time.tv_sec) <<
-                      "   :   " << (end_time.tv_nsec - start_time.tv_nsec)\
+                      ((end_time.tv_sec - start_time.tv_sec) * (long)1e9 + \
+                      (end_time.tv_nsec - start_time.tv_nsec) / 1000000000.0) \
                       << " seconds \n"
 
 #else
