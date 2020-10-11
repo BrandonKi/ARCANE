a99314cb733ef477cbd596b4e0e246c395d0b0f5
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-10-10 21:55:07 -0400

    added more tests

--- Compiler/src/Main.cpp
+++ Compiler/src/Main.cpp
@@ -1,11 +1,11 @@
 
 
 /**
- * @brief The main function and entry point
- *          of the compiler front-end 
+ * @brief The main function and entry point of the compiler front-end 
  * 
  * @return [int] exit code (should not return)
  */
 int main(){
 
 }
+
