d8df0605db5c07a9d6331489012bb10c5e48f79c
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-10-22 22:52:23 -0400

    initial commit

---
+++ Compiler/include/Common.h
@@ -0,0 +1,18 @@
+#include <string>
+#include <cstdint>
+
+struct ARGS{
+    std::string output_filepath; 
+    std::string filepath;
+    bool optimize;
+}args;
+
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int32_t  i32;
+typedef int64_t  i64;
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
