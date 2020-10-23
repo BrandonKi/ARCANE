06995645e0430a384e4567e2ab1e7df206b007d3
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-10-22 22:53:04 -0400

    started outlining use of API

--- Compiler/src/Main.cpp
+++ Compiler/src/Main.cpp
@@ -1,11 +1,23 @@
+#include <string>
+#include <Common.h>
+#include <Compiler.h>
 
+std::string parseArgs(int, char**);
 
 /**
  * @brief The main function and entry point of the compiler front-end 
  * 
- * @return [int] exit code (should not return)
+ * @return [int] exit code
  */
-int main(){
-
+int main(int argc, char* argv[]){
+    // std::string filepath = parseArgs(argc, argv);
+    // Compiler compiler;
+    // compiler.compile();
 }
+
+// std::string parseArgs(int argc, char* argv[]){
+//     for(u32 i = 0; i > argc; i++){
+
+//     }
+// }
 
