03dcf5aac4e6f26a62eeff5ff2e17a840acb7d75
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-10-26 14:18:32 -0400

    move typedefs to top

--- Compiler/include/Common.h
+++ Compiler/include/Common.h
@@ -6,6 +6,15 @@
 
 #include <pLog.h>
 
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int32_t  i32;
+typedef int64_t  i64;
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
 inline struct ARGS{
     std::string filepath;
     std::string output_filepath; 
@@ -23,13 +32,7 @@ struct Token{
 
 enum TokenKind {    // It's named TokenKind instead of TokenType because windows.h stole TokenType from me :(
     /* TODO fill this out */
-};
 
-typedef int8_t   i8;
-typedef int16_t  i16;
-typedef int32_t  i32;
-typedef int64_t  i64;
-typedef uint8_t  u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+    /* Types */
+
+};
