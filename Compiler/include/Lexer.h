eb7873ca9c43f7d597068f09ee90f5d34a01603a
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-10-27 01:15:47 -0400

    started lexing number literals

--- Compiler/include/Lexer.h
+++ Compiler/include/Lexer.h
@@ -4,10 +4,24 @@ class Lexer{
 
     public:
         Lexer(const std::string&);
-        Token next();
-        Token peekNext();
+        Token* next();
+        Token* peekNext();
 
     private:
         const std::string& data;
+        u32 index;
+        u32 lineNum;
+        u32 charNum;
+
+        Token* number();
+
+        constexpr static inline bool isDigit(char c){
+            return c >= 48 && c <= 57;
+        }
+
+        constexpr static inline bool isAlpha(char c){
+            return (c >= 65 && c <= 90) || (c >= 97 && c <= 122) || c == 95;
+        }
+
 
 };
--- Compiler/src/Lexer.cpp
+++ Compiler/src/Lexer.cpp
@@ -1,11 +1,75 @@
 #include "Lexer.h"
 
 Lexer::Lexer(const std::string& data):
-    data(data)
+    data(data), index(0), lineNum(0), charNum(0)
 {
 
 }
 
 Token Lexer::next(){
-
+    Token temp = std::move(peekNext());
+    index++;
+    return std::move(temp);
 }
+
+inline Token Lexer::peekNext(){
+    switch(data[index]){
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            return number_lit();
+        // default:
+        //     peekNext();
+    }
+    return nullptr;
+}
+
+inline Token Lexer::number(){
+    std::string num;
+    bool isFloat = false;
+    bool isHex = false;
+    bool isInt = true;
+    if(index+1 == 'x')
+        isHex = true;
+    while(isDigit(data[index])){
+        index++;
+        charNum++;
+        switch(data[index]){
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                num.append(data[index]);
+                break;
+            case '.':
+                if(isFloat)
+                    continue;   //TODO print error message because two "." are present in number literal 
+                isFloat = true;
+                isHex = false;
+                isInt = false;
+                break;
+            default:
+                break;
+        }
+    }
+    if(isInt)
+        return Token();
+    else if(isFloat)
+        return Token();
+    else
+        return Token();
+}
+
