6ef410577dbca076ff099e9a2aac9695a11bc78b
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-11-02 01:51:39 -0500

    removed MemoryPool for now

--- Compiler/include/MemoryPool.h
+++
@@ -1,6 +0,0 @@
-#include "Common.h"
-
-template <typename T>
-class MemoryPool {
-    /* deleted for now because it provides no benefit other than performance and I can't even generate code yet*/
-};
