187a6e92a04051f97a9c4870cff2bb3ab1d8605c
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-11-02 22:52:39 -0500

    basic identifier implementation

--- Compiler/include/Lexer.h
+++ Compiler/include/Lexer.h
@@ -2,6 +2,16 @@
 
 class Lexer{
 
+    #define CASE_DIGIT case '0': case '1': case '2': case '3': case '4': case '5': \
+                       case '6': case '7': case '8': case '9'
+
+    #define CASE_ID case 'A':case 'B':case 'C':case 'D':case 'E':case 'F':case 'G':case 'H':case 'I': \
+                    case 'J':case 'K':case 'L':case 'M':case 'N':case 'O':case 'P':case 'Q':case 'R': \
+                    case 'S':case 'T':case 'U':case 'V':case 'W':case 'X':case 'Y':case 'Z':case 'a': \
+                    case 'b':case 'c':case 'd':case 'e':case 'f':case 'g':case 'h':case 'i':case 'j': \
+                    case 'k':case 'l':case 'm':case 'n':case 'o':case 'p':case 'q':case 'r':case 's': \
+                    case 't':case 'u':case 'v':case 'w':case 'x':case 'y':case 'z':case '_'
+
     public:
         Lexer(const std::string&);
         void lex();
@@ -13,10 +23,16 @@ class Lexer{
         const std::string& data;
         std::vector<Token*> tokens;
         u32 index;
-        u32 lineNum;
-        u32 charNum;
+        u32 line;
+        u32 col;
+        ErrorHandler error;
 
         Token* number_lit();
+        Token* identifier();
+
+        Token* createToken(std::string&, TokenKind, u32);
+
+        void printTokens();
 
         constexpr static inline bool isDigit(char c) noexcept {
             return c >= 48 && c <= 57;
--- Compiler/src/Lexer.cpp
+++ Compiler/src/Lexer.cpp
@@ -1,47 +1,47 @@
 #include "Lexer.h"
 
 Lexer::Lexer(const std::string& data):
-    data(data), tokens(), index(0), lineNum(1), charNum(1)
+    data(data), tokens(), index(0), line(1), col(1), error(data)
 {
+    error.log(ErrorMessage{FATAL, new Token{std::string("100"), (TokenKind)0, 0, 0}, std::string("filename.txt"), std::string("invalid token")});
+    error.flush();
     tokens.reserve(100);
 }
 
 void Lexer::lex() {     // TODO return Token* instead
+
     while(index < data.size()){
+
         switch(currentChar()){
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                printTokenln(*number_lit());
+            CASE_DIGIT:
+                tokens.push_back(number_lit());
+                break;
+            CASE_ID:
+                tokens.push_back(identifier());
                 break;
             case '\n':
-                lineNum++;
-                charNum = 0;
+                line++;
+                col = 0;
                 break;
             default:
                 break;
         }
-            charNum++;
+            col++;
             index++;
     }
+
+    printTokens();
 }
 
 
 Token* Lexer::number_lit(){
     std::string num;
-    num.reserve(10);
-    u32 charPos = charNum;
+    u32 currentCol = col;
     bool isFloat = false;
     bool isInt = true;
-    if(peekNextChar() == 'x'){
+    if(peekNextChar() == 'x'){  //TODO implement hex literal lexing
         index++;
+        col++;
         isInt = false;
         isFloat = false;
     }
@@ -49,29 +49,48 @@ Token* Lexer::number_lit(){
         num.push_back(currentChar());
 
         if(peekNextChar() == '.'){
-            if(isFloat)
-                continue;   //TODO print error message because two "." are present in number literal 
+            // if(isFloat) //TODO print error message because two "." are present in number literal
             isFloat = true;
             isInt = false;
+
             num.push_back(nextChar());
         }
-        else{
-            //TODO error here. Invalid character in literal
-        }
-        charNum++;
+
+        col++;
         index++;
     }
 
+    col--;
     index--;
 
     if(isInt)
-        return new Token{num, ARC_INT_LIT, lineNum, charPos};
+        return createToken(num, ARC_INT_LIT, currentCol);
     else if(isFloat)
-        return new Token{num, ARC_FLOAT_LIT, lineNum, charPos};
+        return createToken(num, ARC_FLOAT_LIT, currentCol);
     else{
         //TODO convert hex literal to int or float literal
-        return new Token{num, ARC_INT_LIT, lineNum, charPos};
+        return createToken(num, ARC_INT_LIT, currentCol);
     }
+}
+
+Token* Lexer::identifier(){
+
+    std::string id;
+
+    u32 currentCol = col;
+
+    while(isAlpha(currentChar())  || isDigit(currentChar())){
+        id.push_back(currentChar());
+        col++;
+        index++;
+    }
+
+    col--;
+    index--;
+
+    if(keywords.find(id) != keywords.end())
+        return createToken(id, keywords.find(id)->second, currentCol);
+    return createToken(id, ARC_ID, currentCol);
 }
 
 inline char Lexer::currentChar(){
@@ -79,9 +98,19 @@ inline char Lexer::currentChar(){
 }
 
 inline char Lexer::nextChar(){
+    col++;
     return data[++index];
 }
 
 inline char Lexer::peekNextChar(){   
     return data[index+1];
+}
+
+inline Token* Lexer::createToken(std::string& id, TokenKind kind, u32 currentCol){
+    return new Token {id, kind, line, currentCol};
+}
+
+void Lexer::printTokens(){
+    for(Token*& tkn : tokens)
+        printTokenln(*tkn);
 }
