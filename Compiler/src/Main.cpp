b20afc1352a114390c2489ae7fd25b62d1886bd0
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-12-15 00:40:25 -0500

    small edit

--- Compiler/src/Main.cpp
+++ Compiler/src/Main.cpp
@@ -5,12 +5,6 @@
 
 void parseArgs(int, char* argv[]);
 
-/**
- * @brief The main function and entry point of the compiler front-end
- * parses cmd args and calls compile()
- * 
- * @return [int] exit code
- */
 int main(int argc, char* argv[]){
 
     init_pLog();        // init pLog lib
@@ -18,7 +12,7 @@ int main(int argc, char* argv[]){
     parseArgs(argc, argv);
 
     Compiler compiler;
-    compiler.compile(compiler.filedata());
+    compiler.compile();
     #ifdef _DEBUG
     std::cin.get();
     #endif
@@ -43,6 +37,6 @@ void parseArgs(int argc, char* argv[]){         //TODO implement the rest of the
             args.optimize = true;
         //if(str == "-out")
     }
-    args.output_filepath = "out.exe";
-    args.filepath = argList.back();
+    args.output_path = "out.exe";
+    args.path = argList.back();
 }
