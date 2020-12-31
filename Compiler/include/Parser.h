d62a59aeac734cd2c58aec3553a569aee2ed1a5e
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-12-30 20:48:49 -0500

    basic first implementation for expression parsing

--- Compiler/include/Parser.h
+++ Compiler/include/Parser.h
@@ -1,4 +1,4 @@
-#include "AST.h"
+#include "SymbolTable.h"
 
 class Parser{
 
@@ -20,6 +20,10 @@ class Parser{
         Token* Parser::currentToken();
         Token* Parser::nextToken();
         Token* Parser::peekNextToken();
+
+        bool Parser::isOperator(TokenKind);
+        bool Parser::isUnaryOperator(TokenKind kind);
+        u8 Parser::precedence(TokenKind kind);
 
         Type Parser::tokenKind2Type(TokenKind);
 };
--- Compiler/src/Parser.cpp
+++ Compiler/src/Parser.cpp
@@ -49,6 +49,7 @@ Decl* Parser::parseDecl(){
 
     }
     else if(nextToken()->kind == ARC_COLON){
+        nextToken();
         Type type = tokenKind2Type(currentToken()->kind);
         switch(currentToken()->kind){
             case ARC_I8:
@@ -67,7 +68,8 @@ Decl* Parser::parseDecl(){
                 if(peekNextToken()->kind == ARC_SEMICOLON){
                     //TODO definition but no declaration
                 }
-                else if(peekNextToken()->kind == ARC_EQUAL){
+                else if(peekNextToken()->kind == ARC_ASSIGN){
+                    nextToken();
                     nextToken();
                     return ast.newDeclNode(startPos, id, type, parseExpr());
                 }
@@ -77,13 +79,13 @@ Decl* Parser::parseDecl(){
                 break;
             default:
                 //TODO syntax error here
-                errorLog.push(ErrorMessage{});
+                errorLog.push(ErrorMessage{FATAL, currentToken(), args.path, "INVALID TOKEN"});
                 break;
         }
     }
     else{
         //TODO syntax error here
-        errorLog.push(ErrorMessage{});
+        errorLog.push(ErrorMessage{FATAL, currentToken(), args.path, "INVALID TOKEN"});
     }
     return new Decl{};
 }
@@ -95,11 +97,77 @@ Expr* Parser::parseExpr(){
     std::vector<Token*> result;
     std::vector<Token*> stack;
 
+    /**
+     * shunting yard algorithm
+     */
     while(currentToken()->kind != ARC_SEMICOLON){
-        if(peekNextToken()->kind == ARC_INT_LIT){
+        if(currentToken()->kind == ARC_INT_LIT){
+            result.push_back(currentToken());
+        }
+        else if(currentToken()->kind == ARC_ID){    //TODO differentiate between functions and variables
+            result.push_back(currentToken());
+        }
+    else if(isOperator(currentToken()->kind)){  //TODO support unary operators also
+            while ((!stack.empty()) && (isOperator(stack.back()->kind)) && ((precedence(stack.back()->kind) > precedence(currentToken()->kind)) || (precedence(stack.back()->kind) == precedence(currentToken()->kind))) && (stack.back()->kind != ARC_OPEN_PAREN)){
+                result.push_back(stack.back());
+                stack.pop_back();
+            }
+            stack.push_back(currentToken());
+        }
+        else if(currentToken()->kind == ARC_OPEN_PAREN){
+            stack.push_back(currentToken());
+        }
+        else if(currentToken()->kind == ARC_CLOSE_PAREN){
+            while(stack.back()->kind != ARC_OPEN_PAREN){
+                result.push_back(stack.back());
+                stack.pop_back();
+                if(stack.empty()){
+                    //TODO emit error message for mismatched parentheses
+                }
+            }
+            stack.pop_back();
+        }
+        nextToken();
+    }
+
+    while(!stack.empty()){
+        result.push_back(stack.back());
+        stack.pop_back();
+    }
+
+    /**
+     * convert the vector of Tokens into a Expr which will represent a tree
+     */
+    std::vector<Expr*> conversionStack;
+    for(int i = 0; i < result.size(); i++){
+        Token* token = result[i];
+        if(isOperator(token->kind)){
+            if(isUnaryOperator(token->kind)){
+                // conversionStack.push_back(token);
+
+            }
+            else{
+                Expr* operand1 = conversionStack.back();
+                conversionStack.pop_back();
+                Expr* operand2 = conversionStack.back();
+                conversionStack.pop_back();
+                conversionStack.push_back(ast.newExprNode_binExpr(token->pos, token->kind, operand1, operand2));
+            }
+        }
+        else{
+            switch(result[i]->kind){
+                case ARC_INT_LIT:
+                    conversionStack.push_back(ast.newExprNode_intLiteral(token->pos, strtoll(token->data, nullptr, 10)));
+                    break;
+                case ARC_ID:
+                    conversionStack.push_back(ast.newExprNode_variable(token->pos, token->data));
+                    break;
+            }
             
         }
+        printTokenln(result[i]);
     }
+    return conversionStack.back();
 }
 
 inline Token* Parser::currentToken(){
@@ -107,14 +175,23 @@ inline Token* Parser::currentToken(){
 }
 
 inline Token* Parser::nextToken(){
+    if(index + 1 >= tokens.size()){
+        errorLog.push(ErrorMessage{FATAL, currentToken(), args.path, "Reached EOF while parsing"});
+        errorLog.flush();
+    }
+
     return tokens[++index];
 }
 
 inline Token* Parser::peekNextToken(){
+    if(index + 1 >= tokens.size()){
+        errorLog.push(ErrorMessage{FATAL, currentToken(), args.path, "Reached EOF while parsing"});
+        errorLog.flush();
+    }
     return tokens[index + 1];
 }
 
-Type Parser::tokenKind2Type(TokenKind tkn){
+inline Type Parser::tokenKind2Type(TokenKind tkn){
     switch(tkn){
         case ARC_I8:
             return TYPE_I8;
@@ -144,4 +221,64 @@ Type Parser::tokenKind2Type(TokenKind tkn){
             return TYPE_ARR;
     }
     return (Type)-1;
+}
+
+bool Parser::isOperator(TokenKind kind){
+    return kind >= 49;  //FIXME this needs to change if the TokenKind enum changes
+}
+
+bool Parser::isUnaryOperator(TokenKind kind){
+    return 
+        kind == ARC_NEGATE || kind == ARC_NOT || kind == ARC_PRE_INCREMENT || 
+        kind == ARC_POST_INCREMENT || kind == ARC_PRE_DECREMENT || kind == ARC_POST_DECREMENT;
+}
+
+u8 Parser::precedence(TokenKind kind){
+
+    switch(kind){
+        case ARC_PRE_INCREMENT:
+        case ARC_POST_INCREMENT:
+        case ARC_PRE_DECREMENT:
+        case ARC_POST_DECREMENT:
+        case ARC_NOT:
+        case ARC_NEGATE:
+            return 7;
+        case ARC_DIV:
+        case ARC_MUL:
+        case ARC_MOD:
+            return 6;
+        case ARC_ADD:
+        case ARC_SUB:
+            return 5;
+        case ARC_BIN_OR:
+        case ARC_BIN_AND:
+        case ARC_LEFT_SHIFT:
+        case ARC_RIGHT_SHIFT:
+        case ARC_XOR:
+            return 4;
+        case ARC_LOGICAL_OR:
+        case ARC_LOGICAL_AND:
+            return 3;
+        case ARC_LESSER_EQUAL:
+        case ARC_GREATER_EQUAL:
+        case ARC_EQUAL:
+        case ARC_LESSER:
+        case ARC_GREATER:
+            return 2;
+        case ARC_ADD_EQUAL:
+        case ARC_SUB_EQUAL:
+        case ARC_DIV_EQUAL:
+        case ARC_MUL_EQUAL:
+        case ARC_MOD_EQUAL:
+        case ARC_OR_EQUAL:
+        case ARC_AND_EQUAL:
+        case ARC_NOT_EQUAL:
+        case ARC_LEFT_SHIFT_EQUAL:
+        case ARC_RIGHT_SHIFT_EQUAL:
+        case ARC_XOR_EQUAL:
+            return 1;
+        case ARC_ASSIGN:
+        case ARC_INFER:
+            return 0;
+    }
 }
