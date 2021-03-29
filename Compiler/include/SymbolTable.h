cc18aeaa913eefc2d354da82e9e8d44a0aaa66d1
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2021-03-29 15:38:22 -0400

    implemented scopes

--- Compiler/include/SymbolTable.h
+++ Compiler/include/SymbolTable.h
@@ -13,31 +13,38 @@ struct Symbol {
 
 
 class SymbolTable {
-    using pair = std::pair<astring, Symbol>;
-    using hash_map =
-        std::unordered_map<
-            astring,
-            Symbol,
-            std::hash<astring>,
-            std::equal_to<astring>,
-            arena_allocator<
-                std::pair<
-                    const astring,
-                    Symbol
-                >
-            >
-        >;
-
     public:
+
+        using pair = std::pair<astring, Symbol>;
+        using hash_map =
+            std::unordered_map<
+                astring,
+                Symbol,
+                std::hash<astring>,
+                std::equal_to<astring>,
+                arena_allocator<
+                    std::pair<
+                        const astring,
+                        Symbol
+                    >
+                >
+            >;
+        SymbolTable();
         void addSymbol(astring&, SymbolType, Type);
         bool has(astring&);
+        bool scopeHas(astring&);
+        SymbolType getKind(astring&);
         bool isFunction(astring&);
         bool isVariable(astring&);
+        void pushScope();
+        void popScope();
 
     private:
 
-        hash_map table;
-
-
+        hash_map global;
+        std::vector<hash_map, arena_allocator<hash_map>> table;
+                
+        pair get(astring&);
+        hash_map& currentScope();
 
 };
--- Compiler/src/SymbolTable.cpp
+++ Compiler/src/SymbolTable.cpp
@@ -1,17 +1,67 @@
 #include "SymbolTable.h"
 
-void SymbolTable::addSymbol(astring& id, SymbolType type, Type arc_type) {
-    table.insert(pair(id, Symbol{type, arc_type}));
+using pair = std::pair<astring, Symbol>;
+using hash_map =
+    std::unordered_map<
+        astring,
+        Symbol,
+        std::hash<astring>,
+        std::equal_to<astring>,
+        arena_allocator<
+            std::pair<
+                const astring,
+                Symbol
+            >
+        >
+    >;
+
+SymbolTable::SymbolTable() {
+    pushScope();
+}
+
+void SymbolTable::addSymbol(astring& id, SymbolType kind, Type arc_type) {
+    currentScope().insert(pair(id, Symbol{kind, arc_type}));
 }
 
 bool SymbolTable::has(astring& id) {
-    return table.contains(id);
+    for(hash_map& map : table)
+        if(map.contains(id))
+            return true;
+    return false;
+}
+
+// check if element exists before calling function
+SymbolType SymbolTable::getKind(astring& id) {
+    for(hash_map& map : table) {
+        auto result = map.find(id);
+        if(result != map.end())
+            return result->second.type;
+    }
+    return VARIABLE;    // never get here
+}
+
+bool SymbolTable::scopeHas(astring& id) {
+    return currentScope().contains(id);
 }
 
 bool SymbolTable::isFunction(astring& id) {
-    return table.contains(id) && table[id].type == FUNCTION;
+    // FIXME this searches for the symbol twice
+    return has(id) && getKind(id) == FUNCTION;
 }
 
 bool SymbolTable::isVariable(astring& id) {
-    return table.contains(id) && table[id].type == VARIABLE;
+    // FIXME this searches for the symbol twice
+    return has(id) && getKind(id) == VARIABLE;
+}
+
+hash_map& SymbolTable::currentScope() {
+    return table.back();
+}
+
+void SymbolTable::pushScope() {
+    table.push_back(hash_map{});
+}
+
+void SymbolTable::popScope() {
+    table.pop_back();
 }
