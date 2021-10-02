ad3eeaaa2b1657bf4058618cc5c6a5147e4811ec
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2021-10-02 18:40:46 -0400

    progress on stage 2 can now compile and run simple program

--- Compiler/include/BytecodeGen.h
+++ Compiler/include/BytecodeGen.h
@@ -15,26 +15,26 @@ class BytecodeGen {
     BytecodeGen();
 
     arcvm::Arcvm gen_project(Project*);
-    void gen_file(File*);
+    void gen_file(File*, arcvm::Module*);
   private:
     void gen_import(Import*);
-    void gen_function(Function*);
-    void gen_statement(Statement*);
-    void gen_while(WhileStmnt*);
-    void gen_for(ForStmnt*);
-    void gen_if(IfStmnt*);
-    void gen_ret(RetStmnt*);
-    void gen_decl(Decl*);
-    void gen_expr(Expr*);
+    void gen_function(Function*, arcvm::Function*);
+    void gen_statement(Statement*, arcvm::Function*);
+    void gen_while(WhileStmnt*, arcvm::Function*);
+    void gen_for(ForStmnt*, arcvm::Function*);
+    void gen_if(IfStmnt*, arcvm::Function*);
+    void gen_ret(RetStmnt*, arcvm::Function*);
+    void gen_decl(Decl*, arcvm::Function*);
+    arcvm::Value gen_expr(Expr*, arcvm::Function*);
 
-    void gen_immediate(i64);
-    void gen_immediate(f64);
-    void gen_immediate(std::string);
+    arcvm::Value gen_immediate(i64, arcvm::Function*);
+    arcvm::Value gen_immediate(f64, arcvm::Function*);
+    arcvm::Value gen_immediate(std::string*, arcvm::Function*);
 
-    void gen_id(std::string);
-    void gen_fn_call(Expr*);
-    void gen_bin(Expr*);
-    void gen_unary(Expr*);
+    arcvm::Value gen_id(std::string*, arcvm::Function*);
+    arcvm::Value gen_fn_call(Expr*, arcvm::Function*);
+    arcvm::Value gen_bin(Expr*, arcvm::Function*);
+    arcvm::Value gen_unary(Expr*, arcvm::Function*);
 };
 
 #endif
--- Compiler/src/BytecodeGen.cpp
+++ Compiler/src/BytecodeGen.cpp
@@ -1,13 +1,19 @@
 #include "BytecodeGen.h"
 
+BytecodeGen::BytecodeGen() {}
+
 arcvm::Arcvm BytecodeGen::gen_project(Project* project) {
+    arcvm::Arcvm vm;
     for(auto* file: project->files) {
-        gen_file(file);
+        arcvm::IRGenerator ir_gen;
+        auto* mod = ir_gen.create_module();
+        gen_file(file, mod);
+        vm.load_module(mod);
     }
-    return arcvm::Arcvm{};
+    return vm;
 }
 
-void BytecodeGen::gen_file(File* file) {
+void BytecodeGen::gen_file(File* file, arcvm::Module* module) {
     for(auto* import: file->imports){
         // TODO
     }
@@ -17,7 +23,16 @@ void BytecodeGen::gen_file(File* file) {
     }
 
     for(auto* function: file->functions) {
-        // gen_function();
+        auto name = function->id;
+        // TODO convert arg types to std::vector<arcvm::Type>
+        auto args = std::vector<arcvm::Type>{};
+        // TODO convert type_handle to arcvm::Type
+        // auto ret_type = function->return_type;
+        auto ret_type = arcvm::Type::ir_i32;
+        auto ir_function = module->gen_function_def(function->id, args, ret_type);
+        if(function->is_main)
+            ir_function->add_attribute(arcvm::Attribute::entrypoint);
+        gen_function(function, ir_function);
     }
 
 }
@@ -27,89 +42,121 @@ void BytecodeGen::gen_import(Import* import) {
     static_cast<void>(import);
 }
 
-void BytecodeGen::gen_function(Function* function) {
-    // TODO handle all the function arg/return type stuff
-    // and generate with a function id
+void BytecodeGen::gen_function(Function* function, arcvm::Function* ir_gen) {
     for(auto* stmnt: function->body->statements) {
-        gen_statement(stmnt);
+        gen_statement(stmnt, ir_gen);
     }
 }
 
-void BytecodeGen::gen_statement(Statement* statement) {
+void BytecodeGen::gen_statement(Statement* statement, arcvm::Function* ir_gen) {
     switch(statement->type) {
         case WHILE:
-            gen_while(statement->while_stmnt);
+            gen_while(statement->while_stmnt, ir_gen);
             break;
         case FOR:
-            gen_for(statement->for_stmnt);
+            gen_for(statement->for_stmnt, ir_gen);
             break;
         case IF:
-            gen_if(statement->if_stmnt);
+            gen_if(statement->if_stmnt, ir_gen);
             break;
         case RET:
-            gen_ret(statement->ret_stmnt);
+            gen_ret(statement->ret_stmnt, ir_gen);
             break;
         case EXPRESSION:
-            gen_expr(statement->expr);
+            gen_expr(statement->expr, ir_gen);
             break;
         case DECLARATION:
-            gen_decl(statement->decl);
+            gen_decl(statement->decl, ir_gen);
             break;
     }
 }
 
 // TODO
-void BytecodeGen::gen_while(WhileStmnt* while_stmnt) {
+void BytecodeGen::gen_while(WhileStmnt* while_stmnt, arcvm::Function* ir_gen) {
     static_cast<void>(while_stmnt);
 }
 
 // TODO
-void BytecodeGen::gen_for(ForStmnt* for_stmnt) {
+void BytecodeGen::gen_for(ForStmnt* for_stmnt, arcvm::Function* ir_gen) {
     static_cast<void>(for_stmnt);
 }
 
 // TODO
-void BytecodeGen::gen_if(IfStmnt* if_stmnt) {
+void BytecodeGen::gen_if(IfStmnt* if_stmnt, arcvm::Function* ir_gen) {
     static_cast<void>(if_stmnt);
 }
 
 // TODO
-void BytecodeGen::gen_ret(RetStmnt* ret_stmnt) {
-    static_cast<void>(ret_stmnt);
+void BytecodeGen::gen_ret(RetStmnt* ret_stmnt, arcvm::Function* ir_gen) {
+    auto expr_result = gen_expr(ret_stmnt->expr, ir_gen);
+    ir_gen->gen_inst(arcvm::Instruction::ret, {expr_result});
 }
 
-void BytecodeGen::gen_decl(Decl* decl) {
-
-}
-
-void BytecodeGen::gen_expr(Expr* expr) {
+void BytecodeGen::gen_decl(Decl* decl, arcvm::Function* ir_gen) {
 
 }
 
-void BytecodeGen::gen_immediate(i64 immediate) {
-
+arcvm::Value BytecodeGen::gen_expr(Expr* expr, arcvm::Function* ir_gen) {
+    switch(expr->type) {
+        case EXPR_INT_LIT:
+            return gen_immediate(expr->int_literal.val, ir_gen);
+            break;
+        case EXPR_FLOAT_LIT:
+            return gen_immediate(expr->float_literal.val, ir_gen);
+            break;
+        case EXPR_STRING_LIT:
+            return gen_immediate(expr->string_literal.val, ir_gen);
+            break;
+        case EXPR_ID:
+            return gen_id(expr->id.val, ir_gen);
+            break;
+        case EXPR_FN_CALL:
+            return gen_fn_call(expr, ir_gen);
+            break;
+        case EXPR_BIN:
+            return gen_bin(expr, ir_gen);
+            break;
+        case EXPR_UNARY:
+            return gen_unary(expr, ir_gen);
+            break;
+        default:
+            std::cout << "expr gen error\n";
+            return {};
+    }
 }
 
-void BytecodeGen::gen_immediate(f64 immediate) {
-
+arcvm::Value BytecodeGen::gen_immediate(i64 immediate, arcvm::Function* ir_gen) {
+    auto val_ptr = ir_gen->gen_inst(arcvm::Instruction::alloc, {arcvm::Value{arcvm::ValueType::type, arcvm::Type::ir_i32}});
+    ir_gen->gen_inst(arcvm::Instruction::store, {val_ptr, arcvm::Value{arcvm::ValueType::immediate, immediate}});
+    auto val = ir_gen->gen_inst(arcvm::Instruction::load, {val_ptr});
+    return val;
+}
+// TODO
+arcvm::Value BytecodeGen::gen_immediate(f64 immediate, arcvm::Function* ir_gen) {
+    return {};
 }
 
-void BytecodeGen::gen_immediate(std::string immediate) {
-
+// TODO
+arcvm::Value BytecodeGen::gen_immediate(std::string* immediate, arcvm::Function* ir_gen) {
+    return {};
 }
 
-void BytecodeGen::gen_id(std::string id) {
-
+// TODO
+arcvm::Value BytecodeGen::gen_id(std::string* id, arcvm::Function* ir_gen) {
+    return {};
 }
 
-void BytecodeGen::gen_fn_call(Expr* expr) {
-
+// TODO
+arcvm::Value BytecodeGen::gen_fn_call(Expr* expr, arcvm::Function* ir_gen) {
+    return {};
 }
 
-void BytecodeGen::gen_bin(Expr* expr) {
-
+// TODO
+arcvm::Value BytecodeGen::gen_bin(Expr* expr, arcvm::Function* ir_gen) {
+    return {};
 }
 
-void BytecodeGen::gen_unary(Expr* expr) {
-
+// TODO
+arcvm::Value BytecodeGen::gen_unary(Expr* expr, arcvm::Function* ir_gen) {
+    return {};
 }
