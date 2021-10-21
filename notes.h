5b2f1e79a96daa822c69f5d36d8bb73123cdd90c
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2021-10-20 21:24:07 -0400

    add notes

---
+++ notes.h
@@ -0,0 +1,18 @@
+// file for random syntax testing
+// not meant to be included
+#if 0
+
+// var args
+
+// default args
+
+// named args
+
+// ? multiple return values ?
+
+// ? multiple assignment ?
+
+//
+
+
+#endif
